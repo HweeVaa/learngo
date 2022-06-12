a385dac0c63e4efe598fb180b8380e2297872646
HweeVaa <gnlqkgnlgus@gmail.com>
2022-06-13 03:28:55 +0900

    first commit

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	// fmt.Println("When you export it, the fuction should be started with UpperCase!!")
-	// const name string = "Hwee"
-	var name string = "Hwee"
+	// const name string = "Hwee" -> 상수, 변형 불가
+	// var name string = "Hwee" -> 변수, 변형 가능
+	name := "Hwee" //축약형, var name string = "Hwee"와 동일함, 변수만 사용가능하며, 함수 안에서만 사용가능
 	fmt.Println(name)
 }

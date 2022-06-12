6ee12f7eee46f3ab714037db46b698cb85937625
HweeVaa <gnlqkgnlgus@gmail.com>
2022-06-13 03:21:31 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("When you export it, the fuction should be started with UpperCase!!")
+	// const name string = "Hwee"
+	var name string = "Hwee"
+	fmt.Println(name)
+}
